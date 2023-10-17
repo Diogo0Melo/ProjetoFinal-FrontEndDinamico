b922c0b4310801c519e1dd2d68f11c91b75e6c64
Diogo Melo <diogomelo500@gmail.com>
2023-10-17 17:09:13 -0300

    Adiciona login e cadastro

---
+++ public/assets/js/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBdbTFC3iFvWtKsprUmrYQlYTQCcKWiB7Y",
+    authDomain: "frontenddinamico-projetofinal.firebaseapp.com",
+    projectId: "frontenddinamico-projetofinal",
+    storageBucket: "frontenddinamico-projetofinal.appspot.com",
+    messagingSenderId: "766236256676",
+    appId: "1:766236256676:web:bcc32733297b79a0f77931",
+};
+const app = initializeApp(firebaseConfig);
+
+export default app;
