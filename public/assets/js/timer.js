3d8cabbc78144c1fe9fb368f21f760b728b21956
Diogo Melo <diogomelo500@gmail.com>
2023-10-22 18:35:30 -0300

    pequenas mudanças na estilização

--- public/assets/js/timer.js
+++ public/assets/js/timer.js
@@ -1,20 +1,20 @@
-const h = document.getElementById('hora')
-const m = document.querySelector('#minute')
-const s = document.getElementById('second')
+// const h = document.getElementById('hora')
+// const m = document.querySelector('#minute')
+// const s = document.getElementById('second')
 
-const reloguinho = setInterval(function timer(){
-    const k = new Date();
-    const hr = k.getHours()
-    const min = k.getMinutes()
-    const sec = k.getSeconds()
+// const reloguinho = setInterval(function timer(){
+//     const k = new Date();
+//     const hr = k.getHours()
+//     const min = k.getMinutes()
+//     const sec = k.getSeconds()
 
-    if(hr < 10){
-        h.innerHTML=`0${hr}`}
-        else{h.textContent= hr } 
-    if(min < 10){
-        m.innerHTML=`0${min}`}
-        else{m.textContent= min } 
-    if(sec < 10){
-        s.innerHTML=`0${sec}`}
-        else{s.textContent= sec } 
-})
+//     if(hr < 10){
+//         h.innerHTML=`0${hr}`}
+//         else{h.textContent= hr }
+//     if(min < 10){
+//         m.innerHTML=`0${min}`}
+//         else{m.textContent= min }
+//     if(sec < 10){
+//         s.innerHTML=`0${sec}`}
+//         else{s.textContent= sec }
+// })
